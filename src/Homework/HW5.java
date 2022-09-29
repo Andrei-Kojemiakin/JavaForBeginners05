f0f95454284043dce30b7ec49d9a6d24be42cbd9
Andrei-Kojemiakin <kav231@yandex.ru>
2022-09-29 03:09:07 +0300

    Домашнее задание №5 v 0.02

--- src/Homework/HW5.java
+++ src/Homework/HW5.java
@@ -2,18 +2,47 @@ package Homework;
 
 public class HW5 {
 
+    public static void verifyEquals(int expectedResult, int actualResult) {
+        if (expectedResult == actualResult) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        }
+
+    }
+
+    public static String returnDayOfWeek(int namberOfDay) {
+
+        if (0 < namberOfDay || namberOfDay < 8) {
+            if (namberOfDay == 1) {
+                return "Понедельник";
+            }
+            if (namberOfDay == 2) {
+                return "Вторник";
+            }
+            if (namberOfDay == 3) {
+                return "Среда";
+            }
+            if (namberOfDay == 4) {
+                return "Четверг";
+            }
+            if (namberOfDay == 5) {
+                return "Пятница";
+            }
+            if (namberOfDay == 6) {
+                return "Суббота";
+            }
+            if (namberOfDay == 7) {
+                return "Воскресенье";
+            }
+        }
+        return "Error";
+    }
+
 
     public static void main(String[] args) {
 
-
-
-
-
-
-
-
-
-
+        System.out.println(returnDayOfWeek(5));
 
     }
 
