65f3147915ba780eb94b2a9226c26537f4918867
Andrei-Kojemiakin <kav231@yandex.ru>
2022-10-02 14:13:11 +0300

    Домашнее задание №5 v 0.03

--- src/Homework/HW5.java
+++ src/Homework/HW5.java
@@ -3,8 +3,7 @@ package Homework;
 public class HW5 {
 
 
-
-//    public static verify(int expectedResult, int actualResult) {
+    //    public static verify(int expectedResult, int actualResult) {
 //        if (expectedResult == actualResult) {
 //            System.out.println("PASS");
 //        } else {
@@ -12,8 +11,8 @@ public class HW5 {
 //        }
 //
 //    }
-    public static String verify (String expectedResult, String actualResult) {
-        if (expectedResult == actualResult) {
+    public static String verify(String expectedResult, String actualResult) {
+        if (expectedResult.equals(actualResult)) {
             return "PASS";
         } else {
             return "FAIL";
@@ -50,13 +49,24 @@ public class HW5 {
     }
 
 
+    public int averageAge(int age, int age1, int age2) {
+        int average = 0;
+        if (age1 < 1 || age1 > 150 || age < 1 || age > 150 || age2 < 1 || age2 > 150) ;
+            else
+            average = age + age1 + age2 / 3;
+            return average;
+        return 0;
+
+    }
+
+    ;
+
 
     public static void main(String[] args) {
 
         int namber = 1;
-String line =  "_________________________________________________________________________________________________________";
-String task = "Задание " + namber;
-
+        String line = "_________________________________________________________________________________________________________" + "\n";
+        String task = "Задание " + namber;
 
 
         System.out.println(task + '\n');
@@ -64,48 +74,51 @@ String task = "Задание " + namber;
         String expectedResult = "Воскресенье";
         String actualResult = returnDayOfWeek(7);
 
-        System.out.println(verify(expectedResult,actualResult));
+        System.out.println(verify(expectedResult, actualResult));
 
 
         expectedResult = "Error";
         actualResult = returnDayOfWeek(8);
 
-        System.out.println(verify(expectedResult,actualResult));
+        System.out.println(verify(expectedResult, actualResult));
 
         expectedResult = "Понедельник";
         actualResult = returnDayOfWeek(1);
 
-        System.out.println(verify(expectedResult,actualResult));
+        System.out.println(verify(expectedResult, actualResult));
 
         expectedResult = "Вторник";
         actualResult = returnDayOfWeek(2);
 
-        System.out.println(verify(expectedResult,actualResult));
+        System.out.println(verify(expectedResult, actualResult));
 
 
         expectedResult = "Среда";
         actualResult = returnDayOfWeek(3);
 
-        System.out.println(verify(expectedResult,actualResult));
+        System.out.println(verify(expectedResult, actualResult));
 
         expectedResult = "Четверг";
         actualResult = returnDayOfWeek(4);
 
-        System.out.println(verify(expectedResult,actualResult));
+        System.out.println(verify(expectedResult, actualResult));
 
         expectedResult = "Пятница";
         actualResult = returnDayOfWeek(5);
 
-        System.out.println(verify(expectedResult,actualResult));
+        System.out.println(verify(expectedResult, actualResult));
 
         expectedResult = "Суббота";
         actualResult = returnDayOfWeek(6);
 
-        System.out.println(verify(expectedResult,actualResult));
+        System.out.println(verify(expectedResult, actualResult));
 
         System.out.println(line);
-    }
 
+        // ***************************************************************************************************************
+
+
+    }
 
 
 }
